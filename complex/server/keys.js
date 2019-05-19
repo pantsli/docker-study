3b922d0c6b9d623538a0daf22be8ee7af7159913
pantsli <303581489@qq.com>
2019-05-19 23:07:38 +0800

    multi-docker first commit

---
+++ complex/server/keys.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    redisHost: process.env.REDIS_HOST, 
+    redisPort: process.env.REDIS_PORT,
+    psUser: process.env.PGUSER,
+    psHost: process.env.PGHOST, 
+    psDatabase: process.env.PGDATABASE,
+    psPassword: process.env.PGPASSWORD,
+    psPort: process.env.PGPORT
+}
